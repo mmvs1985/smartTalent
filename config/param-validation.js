24547c0571d5b12f955361879d3baebc52355c7d
Daniela Marciukaitis <dmarciukaitis@codigodelsur.com>
2018-08-06 13:54:47 -0300

    Added params validation

--- config/param-validation.js
+++ config/param-validation.js
@@ -1,31 +1,6 @@
 const Joi = require('joi');
 
 module.exports = {
-  // POST /api/users
-  createUser: {
-    body: {
-      username: Joi.string().required(),
-      mobileNumber: Joi.string()
-        .regex(/^[1-9][0-9]{9}$/)
-        .required()
-    }
-  },
-
-  // UPDATE /api/users/:userId
-  updateUser: {
-    body: {
-      username: Joi.string().required(),
-      mobileNumber: Joi.string()
-        .regex(/^[1-9][0-9]{9}$/)
-        .required()
-    },
-    params: {
-      userId: Joi.string()
-        .hex()
-        .required()
-    }
-  },
-
   // POST /api/pets
   createPet: {
     body: {
@@ -35,9 +10,6 @@ module.exports = {
 
   // UPDATE /api/pets/:userId
   updatePet: {
-    body: {
-      name: Joi.string().required()
-    },
     params: {
       petId: Joi.string()
         .hex()
@@ -58,14 +30,6 @@ module.exports = {
       adoptId: Joi.string()
         .hex()
         .required()
-    }
-  },
-
-  // POST /api/auth/login
-  login: {
-    body: {
-      username: Joi.string().required(),
-      password: Joi.string().required()
     }
   }
 };
